0042d11d64c08bfb2801b70de4a37b2f9bf850d2
DuncanMoyo <duncanfmoyo@gmail.com>
2023-06-25 12:47:30 +0000

    added nop opcode

---
+++ nop_opcode.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - entry point
+ * @stack: pointer to a poiter to the stack
+ * @line_number: line number of te current opcode
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
