48c3cccd681d23ada3df891dc020224fe52cec44
DuncanMoyo <duncanfmoyo@gmail.com>
2023-06-25 12:44:47 +0000

    added add opcode

---
+++ add_opcode.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ * add - entry point
+ * @stack: pointer to a pointer to the stack
+ * @line_number: line number of the current opcode
+ */
+
+void add(stack_t **stack, unsinged int line_number)
+{
+	int top_value, next_value;
+	stack_t *top_element;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n"
+				, line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	top_value = (*stack)->n;
+	next_value = (*stack)->next->n;
+
+	(*stack)->next->n = top_value + next_value;
+
+	top_value = *stack;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+
+	free(top_value);
+}
